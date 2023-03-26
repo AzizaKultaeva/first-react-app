21993fa29d6a3212a81ae51ce0555712867a32c2
AzizaKultaeva <akultaeva@gmail.com>
2023-03-26 14:31:57 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 
+const book = {
+  author: 'Chyngyz Aitmatov',
+  title: 'Zhamilia',
+  published: '1958',
+  image: 'https://www.kino-teatr.ru/movie/posters/big/4/1814.jpg'
+};
+
+const books = [
+  { id: 1, title: 'Blackberry', author: 'Sara Johns', isAvailable: true },
+  { id: 2, title: 'Enriques Journey', author: 'Sonia Nazario', isAvailable: true },
+  { id: 3, title: 'Open House', author: 'Patricia J. Williams', isAvailable: false},
+];
+
+function ZineRack() {
+  const listZines = books.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'red' : 'blue'
+      }}
+    >
+      {zine.title}
+      {zine.author}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+function Bookshelf () {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + 'cover'}
+      />
+      }
+    </div>
+  );
+}
+
+
+function MagicButton() {
+  return (
+    <div>
+            <button>Magic Button</button>
+    </div>
+    
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        Hello World! This is my first react app!
+      <header className="App-header">  
+         <Bookshelf />  
+         <ZineRack />  
+         <MagicButton />   
       </header>
     </div>
   );
